957862f159c7a3b19d61bc7b161bab4bb0947e7f
0 <88111864+patam2@users.noreply.github.com>
2021-11-25 14:12:45 +0200

    eemaldasin listi vaate

--- scrape_main.py
+++ scrape_main.py
@@ -1,4 +1,3 @@
-import pymongo
 import re
 
 
@@ -8,26 +7,13 @@ MONGODB_PORT = '27001'
 
 def format_ingrediens(raw: str) -> list:
     """
-    Cleans the ingrediens string by splitting commas, removing information that may not be needed.
+    Eemaldab ebavajalised stringid, võib sisaldada, jne.
     """
-    output = [] 
-    if match := re.search('(toode)* võib (sisaldada)*', raw, flags=re.IGNORECASE): #Eemaldab "toode v6ib sisaldada" asjad
-        raw = raw[:match.start()] #Kuna need marked on lopus ss saab votta alguse
+    print(raw)
+    remove_strings = []
+  
+    if match := re.search('(toode)* võib (sisaldada)*', raw, flags=re.I): #Eemaldab "toode v6ib sisaldada" asjad
+        if match.groups() != (None, None):
+            raw = raw[:match.start()] #Kuna need marked on lopus ss saab votta alguse
 
-    for item in raw.split(','):
-        if '(' in item:
-            output.append(
-                item.split('(', 1)[1].rstrip(')-')
-            )
-
-            if ' '
-
-            continue
-        output.append(item)
-
-    return output
-
-
-format_ingrediens("""
-Suhkur, vahvel 17% (NISUJAHU, rapsiõli, emulgaator (letsitiin), sool, kergitusaine E500, MUNAPULBER, stabilisaator E414), kakaomass, MANDEL, kakaovõi, taimsed rasvad (palmi-, võiseemnikutuumaõli), emulgaator (letsitiin), lõhna- ja maitseaine, vanilliin. Šokolaadi min. 40%. Säilitusaineteta. Võib sisaldada vähesel määral pähkleid, maapähklit, piimatooteid ja seesamiseemneid.
-""")
+    return raw
