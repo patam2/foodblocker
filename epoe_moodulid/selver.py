99b58b60ed2429be838aba918084da03f7305898
0 <88111864+patam2@users.noreply.github.com>
2021-11-25 13:04:05 +0200

    Update selver.py
    allergeenid, defaultdicts ja kiiremad katekooriad

--- epoe_moodulid/selver.py
+++ epoe_moodulid/selver.py
@@ -2,8 +2,10 @@
 """
 
 
-import requests
+from collections import defaultdict
+import os
 import re
+import requests
 
 
 class Selver:
@@ -11,8 +13,15 @@ class Selver:
         self.session = requests.Session()
 
     def get_product_categories(self) -> dict:
+        """
+        Loopib Selveri vue apist product numbritega 2-5,
+        dict {
+            selver_id: selver_name
+        }
+        """
+
         output = {}
-        for product_id in range(50):
+        for product_id in range(2, 6):
             vue_query = '{"query":{"bool":{"filter":{"bool":{"must":[{"terms":{"level":[%d]}},{"terms":{"is_active":[true]}}]}}}}}&size=4000&sort=position:asc' % product_id
             req = self.session.get('https://www.selver.ee/api/catalog/vue_storefront_catalog_et/category/_search?from=0&request=' + vue_query).json()
             if req['hits']['total']['value'] == 0:
@@ -21,16 +30,25 @@ class Selver:
                 output[raw_product_data['_source']['id']] = raw_product_data['_source']['url_path']
         return output
 
-    def get_products_by_category(self, catid: str) -> dict: #Todo -> recursion
-        product_query = open(r'C:\Users\patri\Desktop\Praktiline töö\epoe_moodulid\selver_page_get_query.txt', 'r').read().replace('PRODUCTID', catid)
+    def get_incrediens_by_category(self, catid: str) -> dict:
+        """
+        Võtab vastu selveri kategooria numbri, annab tagasi tooted ja nende allergeenid, koostisosad,
+        dict {
+            product_url {
+                incrediens: str
+                allergens: str
+            }
+        }
+        """
+        product_query = open(fr'{os.getcwd()}\epoe_moodulid\selver_page_get_query.txt', 'r').read().replace('PRODUCTID', catid)
 
-        page, remaining, output = 0, 0, {}
+        page, remaining, output = 0, 0, defaultdict(dict)
 
         req = self.session.get(product_query).json()
-
         for raw_product_data in req['hits']['hits']:
+            url_path = raw_product_data['_source']['url_path']
             if 'product_ingrediens' in raw_product_data['_source']:
-                output[raw_product_data['_source']['url_path']] = {'ingrediens': raw_product_data['_source']['product_ingrediens']}
+                output[url_path]['ingrediens'] = raw_product_data['_source']['product_ingrediens']
+            if 'product_allergens' in raw_product_data['_source']:
+                output[url_path]['allergens'] = raw_product_data['_source']['product_allergens']
         return output
-
-
