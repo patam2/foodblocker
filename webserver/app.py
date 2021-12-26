4a750f255c9f7c4b8d55f54edf89341fa4d5e6fe
0 <88111864+patam2@users.noreply.github.com>
2021-12-27 00:41:56 +0200

    Update app.py
    Fully functional

--- webserver/app.py
+++ webserver/app.py
@@ -1,16 +1,26 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
 
 
 MONGODB_IP = '127.0.0.1'
 MONGODB_PORT = 27017 #TODO: env
 
-mongo_client = MongoClient(host=MONGODB_IP, port=MONGODB_PORT)
+mongo_client = MongoClient(host=MONGODB_IP, port=MONGODB_PORT).filter.selver_products
 flask_client = Flask(__name__)
 
 
 @flask_client.route('/', methods=['POST'])
-def landing():
-    pass
+def landing(): #POST {'urls':[''], 'forbidden': ['']}
+    out = {}
+
+    for url in request.json['urls']:
+        out[url] = []
+        result = mongo_client.find_one({'url_path': url})
+
+        for ingredient in request.json['forbidden']:
+            if ingredient in result['ingrediens']:
+                out[url].append(ingredient)
+
+    return jsonify(out)
 
 flask_client.run()
