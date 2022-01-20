e0048bd5c07bc5f2305fe93e07447feb7f45ddb5
0 <88111864+patam2@users.noreply.github.com>
2022-01-20 17:28:06 +0200

    URL tracking

---
+++ js/background.js
@@ -0,0 +1,11 @@
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (tab.url.endsWith('/cart') && tab.url.startsWith('https://www.selver.ee/')) {
+        console.log('here')
+        chrome.tabs.sendMessage(tabId, {page: "cart"});
+    } else if (tab.url.includes("search") && tab.url.startsWith('https://www.selver.ee/')) {
+        chrome.tabs.sendMessage(tabId, {page: "search"});
+    } else if (tab.url.startsWith('https://www.selver.ee/')) {
+        chrome.tabs.sendMessage(tabId, {page: "product"});
+    };
+});
--- js/worker.js
+++ js/worker.js
@@ -1,32 +1,55 @@
-out_array = []
+var out_array = [];
 
-func = setInterval( 
-    function () {
-        len = document.getElementsByClassName('Product__image-wrapper')
-        if (len.length > 0) {
-            console.log(
-                'Here'
-            )
-            for (i=0; i<len.length; i++) {
-                out_array.push(len[i].href)
-            }
-            $.post({
-                url: 'http://127.0.0.1:5000/',
-                data: JSON.stringify({
-                    'urls': out_array,
-                    'forbidden': ['sinep']
-                }, null, '\t'),
-                contentType: 'application/json',
-                success: function(result) {
-                    for (const [key, value] of Object.entries(result)) {
-                        if (value.length > 0) {
-                            len[key].classList.add("extension-blocked")
-                        }
+function runCartFilter (htmlentity) {
+    console.log('Here')
+    if (htmlentity.length > 0) {
+        for (i=0; i<htmlentity.length; i++) {
+            out_array.push(htmlentity[i].href)
+        }
+        console.log(out_array)
+        $.post({
+            url: 'http://127.0.0.1:5000/',
+            data: JSON.stringify({
+                'urls': out_array,
+                'forbidden': ['sinep']
+            }, null, '\t'),
+            contentType: 'application/json',
+            success: function(result) {
+                for (const [key, value] of Object.entries(result)) {
+                    if (value.length > 0 && htmlentity[key] != undefined) {
+                        htmlentity[key].classList.add("extension-blocked")
                     }
                 }
-            })
-            clearInterval(func)
+            }
+        })
+        out_array = []
+    }
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request) {
+        console.log(request)
+        if (request.page == 'cart') {
+            cartWrapper()
         }
-    },
-    100
-)
+    }
+);
+
+a = []
+  
+
+function cartWrapper (nm) {
+    var func = setInterval(  //lol ma otsisin miks see loopib 24/7 ja see oli selle parast et mai pannud var
+        function () {
+            a.push(func)
+            console.log(nm)
+            len = document.getElementsByClassName('Product__image-wrapper')
+            if (len.length > 0) {
+                runCartFilter(len)
+                clearInterval(func)
+                return
+            }
+        },
+        100
+    )
+}
