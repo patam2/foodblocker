a40490c62448af03964b3d2d4025298b158af561
0 <88111864+patam2@users.noreply.github.com>
2022-01-22 23:35:29 +0200

    Ingredient adding

--- js/settings/handler.js
+++ js/settings/handler.js
@@ -1,12 +1,28 @@
-document.addEventListener("DOMContentLoaded",function(dcle) {
+document.addEventListener("DOMContentLoaded", function (  ) {
+    // Laeb blokeeritud asjad
     chrome.storage.sync.get(['blocked'], function(result) {
-        console.log(result.blocked)
+        if (result != undefined) {
+            result.blocked.forEach(element => {
+                $('tbody').append(`<tr><td>${element}  <button>X</button></td></tr>`)
+            });    
+        }
     }); 
 
+    // Asjade lisamine funtksionaalsus
     var input_box = $('#In')
     input_box.keydown(function (keyevent) {
         if (keyevent.keyCode === 13) {
-            chrome.storage.sync
+            chrome.storage.sync.get(['blocked'], function (result) {
+                let current_list = result.blocked
+                let inpvalue = input_box.val()
+                current_list.push(inpvalue)
+                chrome.storage.sync.set({blocked: current_list})
+                $('tbody').append(`<tr><td>${inpvalue}  <button>X</button></td></tr>`)
+            })
         } 
     })
+
+    // Asjade eemaldamise funtks
+    //console.log($('#Delete'))
+
 }); 
