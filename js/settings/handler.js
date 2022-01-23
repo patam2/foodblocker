dbc871098fc6fc6e259123996fce664dfac606f9
0 <88111864+patam2@users.noreply.github.com>
2022-01-23 18:28:43 +0200

    Update handler.js
    Ingredient removing

--- js/settings/handler.js
+++ js/settings/handler.js
@@ -1,28 +1,43 @@
+var current_list = []
+
 document.addEventListener("DOMContentLoaded", function (  ) {
     // Laeb blokeeritud asjad
     chrome.storage.sync.get(['blocked'], function(result) {
         if (result != undefined) {
+            current_list = result.blocked
             result.blocked.forEach(element => {
-                $('tbody').append(`<tr><td>${element}  <button>X</button></td></tr>`)
+                $('tbody').append(`<tr><td id=item>${element}</td><td><button>X</button></td></tr>`)
             });    
         }
-    }); 
+    });
 
     // Asjade lisamine funtksionaalsus
     var input_box = $('#In')
     input_box.keydown(function (keyevent) {
         if (keyevent.keyCode === 13) {
             chrome.storage.sync.get(['blocked'], function (result) {
-                let current_list = result.blocked
+                current_list = result.blocked
                 let inpvalue = input_box.val()
                 current_list.push(inpvalue)
                 chrome.storage.sync.set({blocked: current_list})
-                $('tbody').append(`<tr><td>${inpvalue}  <button>X</button></td></tr>`)
+
+                row = (`<tr><td id=item>${inpvalue}</td><td><button>X</button></td></tr>`)
+
+                $('tbody').append(row)
             })
         } 
     })
+    $(document).on('click', 'button', function(clicc) {
+        let delete_this = $(this).parent().parent().find('#item').html()
 
-    // Asjade eemaldamise funtks
-    //console.log($('#Delete'))
+        item_index = current_list.indexOf(delete_this)
+        if (item_index != -1) {
+            current_list.splice(item_index, 1)
+        }     
+        
+        chrome.storage.sync.set({blocked: current_list})
 
+        $(this).parent().parent().remove()
+
+   });
 }); 
