96d5c8b33ac73c20c0ae8c7488aca8f9dd38fbe7
0 <88111864+patam2@users.noreply.github.com>
2022-02-07 23:18:27 +0200

    prodcut page

--- js/worker.js
+++ js/worker.js
@@ -31,25 +31,9 @@ function runCartFilter (htmlentity) {
     }
 }
 
-chrome.storage.sync.onChanged.addListener(function () {cartWrapper()})
-
-chrome.runtime.onMessage.addListener(
-    function(request) {
-        console.log(request)
-        if (request.page == 'cart') {
-            cartWrapper()
-        }
-    }
-);
-
-a = []
-  
-
-function cartWrapper (nm) {
+function cartWrapper () {
     var func = setInterval(
         function () {
-            a.push(func);
-            console.log(nm);
             len = document.getElementsByClassName('Product__image-wrapper');
             if (len.length > 0) {
                 runCartFilter(len);
@@ -59,3 +43,50 @@ function cartWrapper (nm) {
         100
     )
 }
+
+
+function runProductFilter ( incredients ) {
+    let koostised_allergeenid = "";
+    for (var i=0; i<incredients.length; i++ ) {
+        var content = incredients[i].innerHTML.toLowerCase()
+        if (content.search('(toode)*( )*(võib)+( )*(sisaldada)*') != -1) {
+            koostised_allergeenid += (' ', content.split('võib')[0])
+        }
+        else {
+            //check for headimng
+        }
+    }
+    // kui ei ole voib sisaldada
+    console.log(koostised_allergeenid)
+}
+
+
+function productWrapper ( ) {
+    let func = setInterval(
+        function() {
+            len = document.getElementsByClassName('AttributeAccordion__content')
+            console.log(len)
+            if (len.length > 0) {
+                runProductFilter(len)
+                clearInterval(func)
+            }
+        },
+        100
+    )
+}
+
+chrome.storage.sync.onChanged.addListener(function () {cartWrapper()})
+
+chrome.runtime.onMessage.addListener(
+    function(request) {
+        console.log(request)
+        if (request.page == 'cart') {
+            cartWrapper()
+        }
+        if (request.page == 'product') {
+            productWrapper()
+        }
+    }
+);
+
+a = []
--- scrape_main.py
+++ scrape_main.py
@@ -29,13 +29,10 @@ def format_ingrediens(raw: str) -> list:
 
 
 for cat_id, cat_name in selver_client.get_product_categories().items():
-    try:
-        gathered_incrediens = selver_client.get_incrediens_by_category(
-            cat_id, 
-            output = []
-        ) 
-    except:
-        continue
+    gathered_incrediens = selver_client.get_incrediens_by_category(
+        cat_id, 
+        output = []
+    ) 
 
     if gathered_incrediens == []:
         continue
