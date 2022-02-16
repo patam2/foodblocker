11afb937803645750b4702dddecc13fa26950112
0 <88111864+patam2@users.noreply.github.com>
2022-02-16 19:44:29 +0200

    Update app.py

--- webserver/app.py
+++ webserver/app.py
@@ -1,6 +1,4 @@
 from flask import Flask, request, jsonify
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 from pymongo import MongoClient
 import time
 
@@ -11,10 +9,9 @@ MONGODB_PORT = 27017 #TODO: env
 mongo_client = MongoClient(host=MONGODB_IP, port=MONGODB_PORT).filter.selver_products
 flask_client = Flask(__name__)
 
-limiter = Limiter(flask_client, key_func=get_remote_address) #avoid double page loads
+
 
 @flask_client.route('/', methods=['POST'])
-@limiter.limit("1/s")
 def landing(): #POST {'urls':[''], 'forbidden': ['']}
     t = time.time()
     out = {}
@@ -29,7 +26,6 @@ def landing(): #POST {'urls':[''], 'forbidden': ['']}
             if (ingredient in result['ingrediens'] 
                     or ingredient in result['allergens']):
                 out[enum].append(ingredient)
-
     #print(out)
     resp = jsonify(out)
     print(f'Request finished in {time.time()-t:.3f}s')
